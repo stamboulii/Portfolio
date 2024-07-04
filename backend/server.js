f3ea174fd77e6d1a56501c67f91c640eea066183
hazarnenni <116651930+stamboulii@users.noreply.github.com>
2024-07-04 01:53:25 +0100

    docker-SetUp
    docker-SetUp

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// MongoDB connection
+const mongoURI = 'mongodb://mongo:27017/portfolio';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
